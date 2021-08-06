0aa42cc5f80e0b8e0afffae179433b5ac5d49339
Miloš Šimek <tjsnake@me.com>
2021-08-06 15:32:40 +0200

    Adds loading screen

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 "use strict"
+
+function onAppLoad() {
+    //Deactivate loading screen
+    const lScreen = document.querySelector(".loading-screen")
+    lScreen.style.display = "none"
+
+    const loadingElem = document.querySelector(".loading-screen p")
+    loadingElem.style.animationPlayState = "paused"
+
+
+}
+
+window.addEventListener("load", onAppLoad)
