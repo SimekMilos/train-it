ba16bfd3f0f70c8e5c201dc0dc1384bb737cf09e
Miloš Šimek <tjsnake@me.com>
2021-08-10 23:16:12 +0200

    Adds control for displaying/hiding settings

--- scripts/main.js
+++ scripts/main.js
@@ -18,11 +18,56 @@ window.addEventListener("load", onAppLoad)
 
 
 // Overview settings activation
-const settingsButton = document.querySelector(".ov-settings")
-const overSettContainer = document.querySelector(".over-sett-container")
+const settingsContainer = document.querySelector(".over-sett-container")
+const overviewSettings = document.querySelector(".comp-over-settings")
 
-function onSettingsActivation() {
-    overSettContainer.classList.toggle("display")
+const settingsButton = document.querySelector(".ov-settings")
+const closeButton = document.querySelector(".ov-sett-close")
+
+
+function onSettingsClick() {
+    const classes = overviewSettings.classList
+
+    if (classes.contains("display") || classes.contains("hide")) {
+        classes.toggle("hide")
+    }
+
+    classes.toggle("display")
+
+    if(classes.contains("display")) {
+        const height = overviewSettings.getBoundingClientRect().height
+
+        let margin = getComputedStyle(overviewSettings).marginTop.slice(0, -2)
+        margin = Number.parseInt(margin)
+
+        settingsContainer.style.height = `${height + margin}px`
+        window.addEventListener("resize", onResize)
+
+    } else {
+        settingsContainer.style.height = "0"
+        window.removeEventListener("resize", onResize)
+    }
 }
 
-settingsButton.addEventListener("click", onSettingsActivation)
+function onCloseButtonClick() {
+    overviewSettings.classList.add("hide")
+    overviewSettings.classList.remove("display")
+    settingsContainer.style.height = "0"
+}
+
+function onResize() {
+    let currentHeight = settingsContainer.style.height
+    currentHeight = Number.parseInt(currentHeight.slice(0, -2))
+
+    const newHeight = overviewSettings.getBoundingClientRect().height
+
+    let margin = getComputedStyle(overviewSettings).marginTop.slice(0, -2)
+    margin = Number.parseInt(margin)
+
+    if (currentHeight !== newHeight) {
+        settingsContainer.style.height = `${newHeight + margin}px`
+    }
+}
+
+settingsButton.addEventListener("click", onSettingsClick)
+closeButton.addEventListener("click", onCloseButtonClick)
