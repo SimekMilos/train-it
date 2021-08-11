430e82dce749f8b86447105cb04ddf7501ef8c10
Miloš Šimek <tjsnake@me.com>
2021-08-11 15:29:37 +0200

    Adds overlay disabling to Overview Component

--- scripts/overview-component.js
+++ scripts/overview-component.js
@@ -1,1 +1,22 @@
 
+const overviewElem = document.querySelector(".comp-overview")
+const classList = overviewElem.classList
+
+/*
+TODO: at places displaying/hiding Overview
+    - disable access when outro animation starts
+    - remove disp-none before intro animation begins
+*/
+
+function onAnimationEnd() {
+    // Displaying - enable access to controls
+    if(classList.contains("display")) {
+        classList.add("enable-access")
+
+    // Hiding - undisplay
+    } else {
+        classList.add("disp-none")
+    }
+}
+
+overviewElem.addEventListener("animationend", onAnimationEnd)
