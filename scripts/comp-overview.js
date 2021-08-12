a320af6c82525e884e58d72b5c204b3754aed74d
Miloš Šimek <tjsnake@me.com>
2021-08-12 20:51:44 +0200

    Moves trainings into template

--- scripts/comp-overview.js
+++ scripts/comp-overview.js
@@ -20,3 +20,29 @@ function onAnimationEnd() {
 }
 
 overviewElem.addEventListener("animationend", onAnimationEnd)
+
+
+// Temporary
+const trainingTemplate = document.querySelector(".ov-training-template")
+const trainingList = document.querySelector(".ov-training-list")
+
+const trainings = document.createDocumentFragment()
+
+for (const [id, text] of [["tr1", "First Training"],
+                          ["tr2", "Second training"],
+                          ["tr3", "Third Training"]]) {
+
+    const training = trainingTemplate.content.cloneNode(true)
+
+    const input = training.querySelector("input")
+    const label = training.querySelector("label")
+    const p = training.querySelector("p")
+
+    input.id = id
+    label.htmlFor = id
+    p.textContent = text
+
+    trainings.append(training)
+}
+
+trainingList.append(trainings)
