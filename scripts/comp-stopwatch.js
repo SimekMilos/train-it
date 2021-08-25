338742b149b93763e3e6e4626afc464d263cd698
Miloš Šimek <tjsnake@me.com>
2021-08-25 11:05:56 +0200

    Adds setHeadingHeight()

--- scripts/comp-stopwatch.js
+++ scripts/comp-stopwatch.js
@@ -3,12 +3,15 @@ import {float, px} from "./tools.js"
 
 const stopwatchComp = document.querySelector(".stopwatch-component")
 
+const headings = document.querySelectorAll(".stopwatch-component h2")
+
 const firstContainer = document.querySelector(".st-first-container")
 const secondContainer = document.querySelector(".st-second-container")
 
 const currentStopwatch = document.querySelector(".st-current-stopwatch")
 const totalStopwatch = document.querySelector(".st-total-stopwatch")
 
+const headingHeightFactor = 0.2
 
 // temporary - activate when first displaying stopwatch
 //           - deactivate when stopwatch is hidden
@@ -22,6 +25,7 @@ window.addEventListener("load", onAppLoad)
 
 function onResize() {
     setComponentHeight()
+    setHeadingHeight()
     setStopwatchSize()
 }
 
@@ -40,11 +44,21 @@ function setComponentHeight() {
     // compute max height
     const maxWatchWidth = (innerWidth - padding - margin) / 2
     const maxWatchHeight = maxWatchWidth / 3.2  // width to height font ratio
-    const maxHeight = (maxWatchHeight + float(compStyles.paddingBottom)) / (1 - 0.2)
-        // 0.2 = size factor of heading height
+    const maxHeight = (maxWatchHeight + float(compStyles.paddingBottom))
+                       / (1 - headingHeightFactor)
 
     if (height > maxHeight) height = maxHeight
     stopwatchComp.style.height = px(height)
+}
+
+function setHeadingHeight() {
+    const compHeight = float(getComputedStyle(stopwatchComp).height)
+    const headingHeight = compHeight * headingHeightFactor
+
+    for (const heading of headings) {
+        heading.style.height = px(headingHeight)
+        heading.style.fontSize = px(.5 * headingHeight)
+    }
 }
 
 function setStopwatchSize() {
