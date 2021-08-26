8b08af0fd7e1f05f442ca78619182a733bb5836c
Miloš Šimek <tjsnake@me.com>
2021-08-26 12:24:23 +0200

    Adds range function

--- scripts/tools.js
+++ scripts/tools.js
@@ -5,3 +5,25 @@ export const int = Number.parseInt
 export function px(value) {
     return `${value}px`
 }
+
+export function* range(startStop, stop, step) {
+    let start;
+
+    // 1 param
+    if (startStop && !stop) {
+        start = 0
+        stop = startStop
+
+    // 2 params
+    } else {
+        start = startStop
+    }
+
+    // 3. param
+    if (!step) step = 1
+
+    // iterate
+    for (let i = start; i < stop; i += step) {
+        yield i
+    }
+}
