baffc9175c8fda22da6433d2a91c83a2caf75373
Miloš Šimek <tjsnake@me.com>
2021-09-04 19:25:45 +0200

    Adds scrolling container

--- scripts/comp-settings.js
+++ scripts/comp-settings.js
@@ -1,6 +1,9 @@
 
+import {float, px} from "./tools.js"
+
 const component = document.querySelector(".settings-component")
 const mainWidnow = document.querySelector(".sett-main-window")
+const scrollContainer = document.querySelector(".sett-scroll-container")
 
 const settingsButton = document.querySelector(".ts-settings")
 const closeButton = document.querySelector(".sett-close")
@@ -9,6 +12,7 @@ let animationInProgress = false
 function onSettingsClick() {
     component.classList.add("display")
     animationInProgress = true
+    setMaxHeight()
 }
 
 function onCloseClick() {
@@ -39,3 +43,23 @@ settingsButton.addEventListener("click", onSettingsClick)
 closeButton.addEventListener("click", onCloseClick)
 component.addEventListener("click", componentCloseClick)
 mainWidnow.addEventListener("animationend", onAnimationEnd)
+
+
+// Sett component max-height
+
+let heightSet = false
+function setMaxHeight() {
+    if (heightSet) return
+    heightSet = true
+
+    const {top: mainTop} = mainWidnow.getBoundingClientRect()
+    const {top: scrollTop} = scrollContainer.getBoundingClientRect()
+
+    const topOffset = scrollTop - mainTop
+    const bottomPadding = float(getComputedStyle(mainWidnow).paddingBottom)
+    const maxHeight = scrollContainer.scrollHeight + topOffset + bottomPadding
+    console.log(topOffset, bottomPadding, scrollContainer.scrollHeight)
+
+    mainWidnow.style.maxHeight = px(maxHeight)
+    console.log(maxHeight)
+}
