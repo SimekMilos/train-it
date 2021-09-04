0816fc9e31aabc7a0233255987b48ec6ad59bf47
Miloš Šimek <tjsnake@me.com>
2021-09-04 15:16:21 +0200

    Adds fade-in/out animations to the component

--- scripts/comp-settings.js
+++ scripts/comp-settings.js
@@ -1,1 +1,28 @@
 
+const component = document.querySelector(".settings-component")
+const mainWidnow = document.querySelector(".sett-main-window")
+
+const settingsButton = document.querySelector(".ts-settings")
+const closeButton = document.querySelector(".sett-close")
+
+
+function onSettingsClick() {
+    component.classList.add("display")
+}
+
+function onCloseClick() {
+    component.classList.add("hide")
+    component.classList.remove("display")
+}
+
+function onAnimationEnd() {
+    if (component.classList.contains("display")) {
+
+    } else {
+        component.classList.remove("hide")
+    }
+}
+
+settingsButton.addEventListener("click", onSettingsClick)
+closeButton.addEventListener("click", onCloseClick)
+mainWidnow.addEventListener("animationend", onAnimationEnd)
