1f43f77c5250058afdc9511153a57f85702eaa2b
Miloš Šimek <tjsnake@me.com>
2021-09-09 21:42:32 +0200

    Improves settings sizing

--- scripts/comp-settings.js
+++ scripts/comp-settings.js
@@ -1,23 +1,16 @@
 
-import {float, px} from "./tools.js"
-
 const component = document.querySelector(".settings-component")
 const mainWidnow = document.querySelector(".sett-main-window")
-const scrollContainer = document.querySelector(".sett-scroll-container")
 
 const settingsButton = document.querySelector(".ts-settings")
 const closeButton = document.querySelector(".sett-close")
 
 let animationInProgress = false
-const heightObserver = new ResizeObserver(setMaxHeight)
 
 
 function onSettingsClick() {
     component.classList.add("display")
     animationInProgress = true
-
-    setMaxHeight()
-    heightObserver.observe(mainWidnow)
 }
 
 function onCloseClick() {
@@ -26,8 +19,6 @@ function onCloseClick() {
 
     mainWidnow.classList.remove("enable-access")
     animationInProgress = true
-
-    heightObserver.disconnect()
 }
 
 function componentCloseClick(e) {
@@ -50,17 +41,3 @@ settingsButton.addEventListener("click", onSettingsClick)
 closeButton.addEventListener("click", onCloseClick)
 component.addEventListener("click", componentCloseClick)
 mainWidnow.addEventListener("animationend", onAnimationEnd)
-
-
-// Sett component max-height
-
-function setMaxHeight() {
-    const {top: mainTop} = mainWidnow.getBoundingClientRect()
-    const {top: scrollTop} = scrollContainer.getBoundingClientRect()
-
-    const topOffset = scrollTop - mainTop
-    const bottomPadding = float(getComputedStyle(mainWidnow).paddingBottom)
-    const maxHeight = scrollContainer.scrollHeight + topOffset + bottomPadding
-
-    mainWidnow.style.maxHeight = px(maxHeight)
-}
