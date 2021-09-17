c81f659bf133cf49d1f3ebb1f08a1e0e246a3504
Miloš Šimek <tjsnake@me.com>
2021-09-17 16:45:33 +0200

    Adds basis for display/hideComponent()

--- scripts/comp-overview/graphics.js
+++ scripts/comp-overview/graphics.js
@@ -1,24 +1,31 @@
 
-const overviewComponent = document.querySelector(".overview-component")
-const compClassList = overviewComponent.classList
+const overviewCom = document.querySelector(".overview-component")
 
 
-// --- Intro/outro animations ---
-
-overviewComponent.addEventListener("animationend", onAnimationEnd)
-
-function onAnimationEnd() {
-    // Displaying - enable access to controls
-    if(compClassList.contains("display")) {
-        compClassList.add("enable-access")
-
-    // Hiding - undisplay
-    } else {
-        compClassList.remove("hide")
-    }
-}
+// --- Displaying component ---
 
 /*
 TODO: at places displaying/hiding Overview
     - disable access when outro animation starts
 */
+
+export function displayComponent() {
+    overviewCom.classList.add("display")
+}
+
+export function hideComponent() {
+
+}
+
+overviewCom.addEventListener("animationend", onAnimationEnd)
+
+function onAnimationEnd() {
+    // Displaying - enable access to controls
+    if(overviewCom.classList.contains("display")) {
+        overviewCom.classList.add("enable-access")
+
+    // Hiding - undisplay
+    } else {
+        overviewCom.classList.remove("hide")
+    }
+}
--- scripts/comp-overview/overview.js
+++ scripts/comp-overview/overview.js
@@ -1,2 +1,5 @@
 
 import * as graphics from "./graphics.js"
+
+export const displayComponent = graphics.displayComponent
+export const hideComponent = graphics.hideComponent
