17a817ee236bdb5cd357274b348efa0b5eb820e4
Miloš Šimek <tjsnake@me.com>
2021-09-17 18:03:46 +0200

    Creates promise based hideComponent()

--- scripts/comp-overview/graphics.js
+++ scripts/comp-overview/graphics.js
@@ -4,28 +4,31 @@ const overviewCom = document.querySelector(".overview-component")
 
 // --- Displaying component ---
 
-/*
-TODO: at places displaying/hiding Overview
-    - disable access when outro animation starts
-*/
+let resolveHide = null
 
 export function displayComponent() {
     overviewCom.classList.add("display")
 }
 
 export function hideComponent() {
+    overviewCom.classList.add("hide")
+    overviewCom.classList.remove("display", "enable-access")
 
+    return new Promise((resolve) => {
+        resolveHide = resolve
+    })
 }
 
-overviewCom.addEventListener("animationend", onAnimationEnd)
-
 function onAnimationEnd() {
-    // Displaying - enable access to controls
+    // Displaying
     if(overviewCom.classList.contains("display")) {
         overviewCom.classList.add("enable-access")
 
-    // Hiding - undisplay
+    // Hiding
     } else {
         overviewCom.classList.remove("hide")
+        resolveHide()
     }
 }
+
+overviewCom.addEventListener("animationend", onAnimationEnd)
