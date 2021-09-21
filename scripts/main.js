37ac83881b0c65c370d7e9a6fba5484397a401e0
Miloš Šimek <tjsnake@me.com>
2021-09-21 19:24:57 +0200

    Adds display/hide screen functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 
-import {wait} from "./tools.js"
+import {wait, waitFor} from "./tools.js"
 import * as overview from "./overview/overview.js"
 
+const initScreen = document.querySelector(".initial-screen")
 
-window.addEventListener("load", onAppLoad)
 
 async function onAppLoad() {
     // Deactivate loading screen
@@ -13,8 +13,33 @@ async function onAppLoad() {
     const loadingElem = document.querySelector(".loading-screen p")
     loadingElem.style.animationPlayState = "paused"
 
-    // Setup overview component
+    // Display initial view
+    displayScreen(initScreen)
     await wait(500)
     overview.loadTrainings()
     overview.display()
+}
+
+window.addEventListener("load", onAppLoad)
+
+
+// --- Screens ---
+
+export async function displayScreen(screen, animationDuration = 0) {
+    screen.style.animationDuration = `${animationDuration / 1000}s`
+
+    await wait(0)       // for animation to pick up duration
+    screen.classList.add("display")
+    await waitFor("animationend", screen)
+}
+
+export async function hideScreen(screen, animationDuration = 0) {
+    screen.style.animationDuration = `${animationDuration / 1000}s`
+
+    await wait(0)       // for animation to pick up duration
+    screen.classList.add("hide")
+    screen.classList.remove("display")
+
+    await waitFor("animationend", screen)
+    screen.classList.remove("hide")
 }
