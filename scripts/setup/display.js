3aa264c0f0dfae0e9930f07545e7cea020033700
Miloš Šimek <tjsnake@me.com>
2021-09-23 00:43:23 +0200

    Adds hide()

--- scripts/setup/display.js
+++ scripts/setup/display.js
@@ -1,5 +1,5 @@
 
-import {float, waitFor} from "../tools.js"
+import {px, float, waitFor} from "../tools.js"
 import * as overview from "../overview/overview.js"
 
 const initialScreen = document.querySelector(".initial-screen")
@@ -10,25 +10,52 @@ const containerStyles = window.getComputedStyle(container)
 
 
 export async function display() {
+    // Start display animation
     component.classList.add("display")
-    component.style.width = getComponentWidth()
     container.classList.add("display")
+    component.style.width = getComponentWidth()
 
-    // Alternative display
-    if(containerStyles.position !== "absolute") {
+    // Start push away efekt (standard display)/ disables shadow (overlay)
+    if(containerStyles.position != "absolute") {
         container.classList.add("animate")
     } else {
         overview.disableShadow()
     }
 
+    // Disable overview component in parallel
     overview.disable(700)
 
+    // Finish display
     await waitFor("animationend", component)
     component.classList.add("enable-access")
+    container.classList.remove("animate")
     component.style.removeProperty("width")
 }
 
+export async function hide() {
+    // Start hide animation
+    component.classList.remove("display", "enable-access")
+    component.classList.add("hide")
+    component.style.width = getComponentWidth()
 
+    container.classList.add("hide")
+    container.classList.remove("display")
+
+    // Start push away efekt in standard display
+    if (containerStyles.position != "absolute") {
+        container.classList.add("animate")
+    }
+
+    // Enable overview component in parallel
+    overview.enable(700)
+    overview.enableShadow()
+
+    // Finish hide
+    await waitFor("animationend", component)
+    component.classList.remove("hide")
+    container.classList.remove("hide", "animate")
+    component.style.removeProperty("width")
+}
 
 function getComponentWidth() {
     let width = initialScreen.clientWidth
