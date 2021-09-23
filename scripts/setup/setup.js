4f357a15e4860ab8fdf651160d600f8dd50047ae
Miloš Šimek <tjsnake@me.com>
2021-09-23 17:34:49 +0200

    Adds create/read functions stubs

--- scripts/setup/setup.js
+++ scripts/setup/setup.js
@@ -1,11 +1,55 @@
 
-export function setupTraining(oldTraining = null) {
+import {range, waitFor} from "../tools.js"
+import * as display from "./display.js"
+
+const buttonCancel = document.querySelector(".ts-cancel")
+
+
+
+export async function setupTraining(oldTraining = null) {
    /* Main component function to create/edit training
 
       oldTraining - training data object/null
       return - Promise of training object/null
    */
 
-   if (!oldTraining) return Promise.resolve({name: "Training x"})
-   else              return Promise.resolve({name: "Training y"})
+   display.display()
+
+   await waitFor("click", buttonCancel)
+
+   display.hide()
+
+   return null
+}
+
+
+
+function createTraining(trainingData) {
+
+}
+
+function readTraining(component) {
+
+}
+
+
+function createGroup(groupData) {
+   /* Both for groups and no-groups */
+
+}
+
+function readGroup(groupElem) {
+   /* Both for groups and no-groups */
+
+}
+
+
+function createExcercise(data) {
+   /* Input - excercise object/null (empty excercise) */
+
+}
+
+function readExcercise(excerciseElem) {
+
+}
 }
