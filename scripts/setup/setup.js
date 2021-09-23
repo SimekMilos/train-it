d15323d57c27944043fd62a84addf3cee3a1c206
Miloš Šimek <tjsnake@me.com>
2021-09-23 17:35:18 +0200

    Adds createExcercise()

--- scripts/setup/setup.js
+++ scripts/setup/setup.js
@@ -2,6 +2,13 @@
 import {range, waitFor} from "../tools.js"
 import * as display from "./display.js"
 
+
+const groupTemplate = document.querySelector(".ts-group-template")
+const noGroupTemplate = document.querySelector(".ts-no-group-template")
+const exerciseTemplate = document.querySelector(".ts-exercise-template")
+const setTemplate = document.querySelector(".ts-set-template")
+
+
 const buttonCancel = document.querySelector(".ts-cancel")
 
 
@@ -47,6 +54,35 @@ function readGroup(groupElem) {
 function createExcercise(data) {
    /* Input - excercise object/null (empty excercise) */
 
+   const exercise = exerciseTemplate.content.cloneNode(true)
+   const name = exercise.querySelector(".ts-exercise-name")
+   const notes = exercise.querySelector(".ts-exercise-notes")
+   const setContainer = exercise.querySelector(".ts-exercise-set-container")
+
+   // Setup excercise
+   if (data) {
+      name.value = data.name
+      notes.textContent = data.notes
+
+      // Setup sets
+      for (const setName of data.sets) {
+         const set = setTemplate.content.cloneNode(true)
+
+         if (setName) {
+            const nameElem = set.querySelector(".ts-set-name")
+            nameElem.value = setName
+         }
+         setContainer.append(set)
+      }
+
+   // Default
+   } else {
+      for (const _ of range(3)) {
+         setContainer.append(setTemplate.content.cloneNode(true))
+      }
+   }
+
+   return exercise
 }
 
 function readExcercise(excerciseElem) {
