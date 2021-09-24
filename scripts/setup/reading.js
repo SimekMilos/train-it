0410fb7022c857d9c32b9d842789d84239d1c719
Miloš Šimek <tjsnake@me.com>
2021-09-24 18:47:01 +0200

    Adds error handling for training reading

--- scripts/setup/reading.js
+++ scripts/setup/reading.js
@@ -1,11 +1,18 @@
 
-function readTraining() {
+export class ReadError extends Error {
+    constructor(message) {
+        super(message)
+        this.name = "ReadError"
+    }
+}
 
+export function readTraining() {
+    return {}
 }
 
 
 function readGroup(groupElem) {
-   /* Both for groups and no-groups */
+    /* Both for groups and no-groups */
 
 }
 
--- scripts/setup/setup.js
+++ scripts/setup/setup.js
@@ -2,6 +2,7 @@
 import {waitForAny} from "../tools.js"
 import * as display from "./display.js"
 import {createTraining, createGroup, createExcercise} from "./creating.js"
+import {readTraining, ReadError} from "./reading.js"
 
 const trainingName = document.querySelector(".ts-training-name")
 const trainingNotes = document.querySelector(".ts-training-notes")
@@ -14,44 +15,59 @@ const buttonSave = document.querySelector(".ts-save")
 
 
 export async function setupTraining(trainingData = null) {
-   /* Main component function to create/edit training
+    /* Main component function to create/edit training
 
       trainingData - training data object/null
       return - Promise of training object/null
-   */
+    */
 
-   let returnData = null
-   let finished = false
+    let returnData = null
+    let finished = false
 
-   // Display component
-   if (trainingData) createTraining(trainingData)
-   await display.display()
+    // Display component
+    if (trainingData) createTraining(trainingData)
+    await display.display()
 
-   // Edit cycle
-   do {
-      const save = await waitForAny(["click", buttonSave,   true],
-                                    ["click", buttonCancel, false])
+    // Edit cycle
+    do {
+        const save = await waitForAny(["click", buttonSave,   true],
+                                      ["click", buttonCancel, false])
 
-      if (save) {
-         // read and validate data
-         // if valid, save to returnData and set finished to true
-      } else {
-         finished = true
-      }
-   } while(!finished)
+        if (save) {
+            finished = true
+            try {
+                returnData = readTraining()
 
-   // Hide component
-   await display.hide()
+                // Copy training settings
+                if (trainingData && trainingData.settings) {
+                    returnData.settings = trainingData.settings
+                }
+            } catch (error) {
+                if (error instanceof ReadError) {
+                    finished = false
+                    alert(error.message)
+                } else throw error
+            }
+        } else {
+            finished = true
+            // warning before closing would require detecting change in data
+            // which would need comparing original data to new data
+            // settings in data should be ignored in comparison
+        }
+    } while(!finished)
 
-   // Delete visual content
-   trainingName.value = ""
-   trainingNotes.textContent = ""
+    // Hide component
+    await display.hide()
 
-   let groups = Array.from(groupContainer.children)
-   groups = groups.slice(1)         // not first element (no-excercise display)
-   for (const group of groups) group.remove()
+    // Delete visual content
+    trainingName.value = ""
+    trainingNotes.textContent = ""
 
-   return returnData
+    let groups = Array.from(groupContainer.children)
+    groups = groups.slice(1)         // not first element (no-excercise display)
+    for (const group of groups) group.remove()
+
+    return returnData
 }
 
 
@@ -61,18 +77,18 @@ buttonAddGroup.addEventListener("click", addGroup)
 buttonAddExcercise.addEventListener("click", addExcercise)
 
 function addGroup() {
-   groupContainer.append(createGroup())
+    groupContainer.append(createGroup())
 }
 
 function addExcercise() {
-   const last = groupContainer.lastElementChild
+    const last = groupContainer.lastElementChild
 
-   // Appends excercise
-   if (last.classList.contains("ts-no-group")) {
-      last.append(createExcercise())
+    // Appends excercise
+    if (last.classList.contains("ts-no-group")) {
+        last.append(createExcercise())
 
-   // Creates no-group container if needed (with 1 excercise)
-   } else {
-      last.after(createGroup({type: "no-group", excercises: [null]}))
-   }
+    // Creates no-group container if needed (with 1 excercise)
+    } else {
+        last.after(createGroup({type: "no-group", excercises: [null]}))
+    }
 }
