a5564e2376743ed3c9473a66601c0d4b30597be4
Miloš Šimek <tjsnake@me.com>
2021-09-25 20:48:41 +0200

    Adds warning while canceling changed training

--- scripts/setup/setup.js
+++ scripts/setup/setup.js
@@ -1,5 +1,5 @@
 
-import {waitForAny} from "../tools.js"
+import {range, waitForAny} from "../tools.js"
 import * as display from "./display.js"
 import {createTraining, createGroup, createExercise} from "./creating.js"
 import {readTraining, ReadError} from "./reading.js"
@@ -32,7 +32,7 @@ export async function setupTraining(trainingData = null) {
     do {
         const save = await waitForAny(["click", buttonSave,   true],
                                       ["click", buttonCancel, false])
-
+        // Saving training
         if (save) {
             finished = true
             try {
@@ -50,11 +50,14 @@ export async function setupTraining(trainingData = null) {
                     alert(error.message)
                 } else throw error
             }
+
+        // Cancel edit
         } else {
-            finished = true
-            // warning before closing would require detecting change in data
-            // which would need comparing original data to new data
-            // settings in data should be ignored in comparison
+            if (hasChanged(trainingData)) {
+                finished = confirm("Do you want to delete unsaved changes?")
+            } else {
+                finished = true
+            }
         }
     } while(!finished)
 
@@ -93,4 +96,72 @@ function addExercise() {
     } else {
         last.after(createGroup({type: "no-group", exercises: [null]}))
     }
+}
+
+function hasChanged(originalTrainingData) {
+    // Test when creating new training
+    if (!originalTrainingData) {
+        if (trainingName.value.trim()) return true
+        if (trainingNotes.value.trim()) return true
+        if (groupContainer.children.length > 1) return true
+
+        return false
+    }
+
+    // Test when editing training
+    // Get current data
+    let current
+    try {
+        current = readTraining()
+    } catch (err) {                    // ReadError == training has been edited
+        if (err instanceof ReadError) return true
+        else throw err
+    }
+
+    // Compare data sets
+    return !areEqual(originalTrainingData, current)
+}
+
+
+// Data equality comparison
+
+function areEqual(first, second) {
+    if (first.name != second.name) return false
+    if (first.notes != second.notes) return false
+
+    // Compare groups
+    const gr1 = first.groups, gr2 = second.groups
+    if (gr1.length != gr2.length) return false
+
+    for (const index of range(gr1.length)) {
+        if (!groupsAreEqual(gr1[index], gr2[index])) return false
+    }
+
+    return true
+}
+
+function groupsAreEqual(first, second) {
+    if (first.type != second.type) return false
+    if (first.name != second.name) return false
+    if (first.notes != second.notes) return false
+
+    // Compare exercises
+    const ex1 = first.exercises, ex2 = second.exercises
+    if (ex1.length != ex2.length) return false
+
+    for (const index of range(ex1.length)) {
+        if (!exercisesAreEqual(ex1[index], ex2[index])) return false
+    }
+
+    return true
+}
+
+function exercisesAreEqual(first, second) {
+    if (first.name != second.name) return false
+    if (first.notes != second.notes) return false
+
+    // Compare sets
+    if (JSON.stringify(first.sets) != JSON.stringify(second.sets)) return false
+
+    return true
 }
