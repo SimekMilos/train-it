7936241218df3093e70824f95907eec0aca8edfc
Miloš Šimek <tjsnake@me.com>
2021-10-01 13:33:04 +0200

    Creates export function

--- scripts/overview-settings/overview-settings.js
+++ scripts/overview-settings/overview-settings.js
@@ -4,6 +4,7 @@ import * as displayFunc from "./display.js"
 const exportButton = document.querySelector(".ovs-export")
 const importButton = document.querySelector(".ovs-import")
 
+const exportFileName = "trainings.train-it"
 
 // --- Public ---
 
@@ -21,7 +22,19 @@ importButton.addEventListener("click", onImport)
 
 
 function onExport() {
+    // Create data file
+    const file = new File([JSON.stringify(localStorage)],
+                          exportFileName,
+                          {type: "application/octet-stream"})
 
+    // Create link to it
+    const a = document.createElement("a")
+    a.href = URL.createObjectURL(file)
+    a.download = exportFileName
+
+    // Save
+    a.click()
+    URL.revokeObjectURL(a.href)
 }
 
 function onImport() {
