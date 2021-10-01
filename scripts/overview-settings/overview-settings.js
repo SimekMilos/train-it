c74416819827d889e3b055f15e7f0af1ff61db25
Miloš Šimek <tjsnake@me.com>
2021-10-01 11:36:29 +0200

    Adds stubs for import/export operations

--- scripts/overview-settings/overview-settings.js
+++ scripts/overview-settings/overview-settings.js
@@ -1,8 +1,29 @@
 
 import * as displayFunc from "./display.js"
 
+const exportButton = document.querySelector(".ovs-export")
+const importButton = document.querySelector(".ovs-import")
+
+
+// --- Public ---
+
 export const isDisplayed = displayFunc.isDisplayed
 export const display     = displayFunc.display
 export const hide        = displayFunc.hide
 
 export const onStorageRemove = displayFunc.setExportState
+
+
+// --- Private ---
+
+exportButton.addEventListener("click", onExport)
+importButton.addEventListener("click", onImport)
+
+
+function onExport() {
+
+}
+
+function onImport() {
+
+}
