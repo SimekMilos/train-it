0b71e35edb2b55e8d86501d1e84d56e1ee0ff26e
Miloš Šimek <tjsnake@me.com>
2021-10-04 03:00:58 +0200

    Puts importing functionality in overlay disabling context

--- scripts/overview-settings/overview-settings.js
+++ scripts/overview-settings/overview-settings.js
@@ -1,5 +1,6 @@
 
-import {waitForAny, dialog, generateTrainingID} from "../tools.js"
+import {waitForAny, dialog,
+        asyncContextManager, generateTrainingID} from "../tools.js"
 
 import * as overview from "../overview/overview.js"
 import * as displayFunc from "./display.js"
@@ -52,52 +53,54 @@ function onExport() {
     URL.revokeObjectURL(a.href)
 }
 
-async function onImport() {
-    // Create file input
-    const input = document.createElement("input")
-    input.type = "file"
-    input.accept = fileExtension
-    input.click()
+function onImport() {
+    overlayContext(async () => {
+        // Create file input
+        const input = document.createElement("input")
+        input.type = "file"
+        input.accept = fileExtension
+        input.click()
 
-    // Wait for user interaction
-    const canceled = await waitForAny(["change", input, false],
-                                      ["click", window, true, true])
-    if (canceled) return
+        // Wait for user interaction
+        const canceled = await waitForAny(["change", input, false],
+                                          ["click", window, true, true])
+        if (canceled) return
 
-    // Load file
-    const reader = new FileReader()
-    reader.readAsText(input.files[0])
-    const success = await waitForAny(["load", reader, true],
-                                     ["errror", reader, false])
-    if (!success) {
-        await dialog("File loading not successful.", "OK")
-        return
-    }
-
-    // Parse file
-    let fileData
-    try {
-        fileData = parseFile(reader.result)
-    } catch (err) {
-        if (err instanceof ParserError) {
-            await dialog("File cannot be imported. Incorrect file format!", "OK")
+        // Load file
+        const reader = new FileReader()
+        reader.readAsText(input.files[0])
+        const success = await waitForAny(["load", reader, true],
+                                         ["errror", reader, false])
+        if (!success) {
+            await dialog("File loading not successful.", "OK")
             return
-        } else throw err
-    }
+        }
 
-    // Ask for action (only if app isn't empty)
-    if (localStorage["training-order"]) {
-        const action = await dialog("You have trainings in the app. \
-            Do you want to add imported trainings after existing ones or \
-            replace them?", "Add", "Replace", "Cancel")
+        // Parse file
+        let fileData
+        try {
+            fileData = parseFile(reader.result)
+        } catch (err) {
+            if (err instanceof ParserError) {
+                await dialog("File cannot be imported. Incorrect file format!", "OK")
+                return
+            } else throw err
+        }
 
-        if (action == "Cancel") return
-        if (action == "Replace") localStorage.clear()
-    }
+        // Ask for action (only if app isn't empty)
+        if (localStorage["training-order"]) {
+            const action = await dialog("You have trainings in the app. \
+                Do you want to add imported trainings after existing ones or \
+                replace them?", "Add", "Replace", "Cancel")
 
-    // Save data
-    mergeData(fileData)
-    overview.loadTrainings()
+            if (action == "Cancel") return
+            if (action == "Replace") localStorage.clear()
+        }
+
+        // Save data
+        mergeData(fileData)
+        overview.loadTrainings()
+    })
 }
 
 function mergeData(fileData) {
@@ -120,3 +123,22 @@ function mergeData(fileData) {
     if (!orderArr.length) throw new Error("Order array empty after import")
     storage["training-order"] = JSON.stringify(orderArr)
 }
+
+// Disables applicaton until importing is finished
+const overlayContext = asyncContextManager(() => {
+    const overlay = document.createElement("div")
+    const style = overlay.style
+
+    style.position = "absolute"
+    style.top = 0
+    style.left = 0
+    style.right = 0
+    style.bottom = 0
+    style.zIndex = 1
+
+    document.body.append(overlay)
+    return overlay
+
+}, (overlay) => {
+    overlay.remove()
+})
