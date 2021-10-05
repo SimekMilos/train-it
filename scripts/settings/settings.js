49038808baf641179e9b7d5d4f5a2de44e274fab
Miloš Šimek <tjsnake@me.com>
2021-10-05 11:41:05 +0200

    Adds open()

--- scripts/settings/settings.js
+++ scripts/settings/settings.js
@@ -1,8 +1,42 @@
 
+import {waitFor} from "../tools.js"
+
+const component = document.querySelector(".settings-component")
+const mainWidnow = component.firstElementChild
+
+const closeButton = document.querySelector(".sett-close")
+
+
+// --- Public ---
+
 export function init(trainingData) {
 
 }
 
 export function destroy() {
 
+}
+
+export async function open() {
+    /* return - true if settings were changed */
+
+    // Display component
+    component.classList.add("display")
+    await waitFor("animationend", mainWidnow)
+    mainWidnow.classList.add("enable-access")
+
+    // Wait for close event
+    do {        // Close by clicking on close button or background
+        const event = await waitFor("click", component)
+    } while (event.target != closeButton && event.target != component)
+
+    // Hide component
+    mainWidnow.classList.remove("enable-access")
+    component.classList.add("hide")
+    component.classList.remove("display")
+
+    await waitFor("animationend", mainWidnow)
+    component.classList.remove("hide")
+
+    return false
 }
--- scripts/training/training.js
+++ scripts/training/training.js
@@ -1,4 +1,6 @@
 
+import * as settings from "../settings/settings.js"
+
 //TODO - edit in screens.js if it should be called when traininData == null
 
 export function init(trainingData) {
@@ -11,4 +13,13 @@ export function destroy() {
 
 export function display() {
 
+}
+
+const settingsButton = document.querySelector(".ts-settings")
+
+settingsButton.addEventListener("click", onSettingsClick)
+
+async function onSettingsClick() {
+    const ret = await settings.open()
+    log("Return value: " + ret)
 }
