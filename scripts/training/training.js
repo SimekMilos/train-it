cfa2d5a1c996655754b0cef5542f2f293d6874dc
Miloš Šimek <tjsnake@me.com>
2021-10-08 16:31:12 +0200

    Adds action interface to training.js

--- scripts/training/training.js
+++ scripts/training/training.js
@@ -1,6 +1,11 @@
 
 //TODO - edit in screens.js if it should be called when trainingData == null
 
+
+// --- Public ---
+
+// Initialization
+
 export function init(trainingData) {
 
 }
@@ -10,5 +15,32 @@ export function destroy() {
 }
 
 export function display() {
+
+}
+
+export function setTimer(timer) {
+
+}
+
+
+// Actions
+
+export function next() {
+    /* return - "set", "pause", null (no next phase) */
+
+    return "set"
+}
+
+export function back() {
+    /* return {
+        phase: "set" / "pause" / null (no previous phase)
+        time: Number    - current time of the previous phase
+        }
+    */
+
+    return { phase: "pause", time: 123 }
+}
+
+export function reset() {
 
 }
