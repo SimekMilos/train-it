7ac1d6c55099f36337fc9dbf72c0c9a64f15b398
Miloš Šimek <tjsnake@me.com>
2021-10-08 20:34:13 +0200

    Adds setCurrentWatchTime()

--- scripts/stopwatch/watches.js
+++ scripts/stopwatch/watches.js
@@ -6,15 +6,24 @@ let currentWatchTime = 0
 let totalWatchTime = 0
 
 
+export function setCurrentWatchTime(time) {
+    currentWatchTime = time
+    currentWatch.textContent = getWatchString(currentWatchTime)
+}
+
+export function resetCurrentWatchTime() {
+    currentWatchTime = 0
+    currentWatch.textContent = "00:00"
+}
+
+export function resetTotalWatchTime() {
+    totalWatchTime = 0
+    totalWatch.textContent = "00:00:00"
+}
+
 export function addToCurrentWatch() {
     currentWatchTime++
-
-    let seconds = currentWatchTime % 60
-    let minutes = ((currentWatchTime - seconds) / 60) % 60
-    seconds = String(seconds).padStart(2, "0")
-    minutes = String(minutes).padStart(2, "0")
-
-    currentWatch.textContent = `${minutes}:${seconds}`
+    currentWatch.textContent = getWatchString(currentWatchTime)
 }
 
 export function addToTotalWatch() {
@@ -30,12 +39,14 @@ export function addToTotalWatch() {
     totalWatch.textContent = `${hours}:${minutes}:${seconds}`
 }
 
-export function resetCurrentWatchTime() {
-    currentWatchTime = 0
-    currentWatch.textContent = "00:00"
-}
 
-export function resetTotalWatchTime() {
-    totalWatchTime = 0
-    totalWatch.textContent = "00:00:00"
+// --- Private ---
+
+function getWatchString(time) {
+    let seconds = currentWatchTime % 60
+    let minutes = ((currentWatchTime - seconds) / 60) % 60
+    seconds = String(seconds).padStart(2, "0")
+    minutes = String(minutes).padStart(2, "0")
+
+    return `${minutes}:${seconds}`
 }
