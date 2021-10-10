a09d75708f9e5dcd2f2a0d061738063937fc7d38
Miloš Šimek <tjsnake@me.com>
2021-10-10 02:30:10 +0200

    Moves spacebar functionality to separate file

---
+++ scripts/stopwatch/spacebar.js
@@ -0,0 +1,70 @@
+
+/* Spacebar button activation functionality */
+
+const buttonStart = document.querySelector(".st-start")
+const buttonStop = document.querySelector(".st-stop")
+const buttonContinue = document.querySelector(".st-continue")
+const buttonNext = document.querySelector(".st-next")
+const buttonFinish = document.querySelector(".st-finish")
+
+
+// --- Interface ---
+
+export function activate() {
+    document.addEventListener("keydown", keyDown)
+    document.addEventListener("keyup", keyUp)
+}
+
+export function deactivate() {
+    document.removeEventListener("keydown", keyDown)
+    document.removeEventListener("keyup", keyUp)
+    removePressAppearance()
+}
+
+
+// --- Private ---
+
+function keyDown(ev) {      // Activates button press appearance
+    if (ev.code != "Space") return
+
+    if (getComputedStyle(buttonStart).display == "block") {
+        buttonStart.classList.add("spacebar-active")
+    }
+
+    if (getComputedStyle(buttonStop).display == "block") {
+        buttonStop.classList.add("spacebar-active")
+    }
+
+    if (getComputedStyle(buttonContinue).display == "block") {
+        buttonContinue.classList.add("spacebar-active")
+    }
+
+    if (getComputedStyle(buttonNext).display == "block") {
+        buttonNext.classList.add("spacebar-active")
+    }
+
+    if (getComputedStyle(buttonFinish).display == "block") {
+        buttonFinish.classList.add("spacebar-active")
+    }
+}
+
+function keyUp(ev) {
+    if (ev.code != "Space") return
+
+    removePressAppearance()
+
+    // Activate button
+    if (getComputedStyle(buttonStart).display == "block") buttonStart.click()
+    if (getComputedStyle(buttonStop).display == "block") buttonStop.click()
+    if (getComputedStyle(buttonContinue).display == "block") buttonContinue.click()
+    if (getComputedStyle(buttonNext).display == "block") buttonNext.click()
+    if (getComputedStyle(buttonFinish).display == "block") buttonFinish.click()
+}
+
+function removePressAppearance() {
+    buttonStart.classList.remove("spacebar-active")
+    buttonStop.classList.remove("spacebar-active")
+    buttonContinue.classList.remove("spacebar-active")
+    buttonNext.classList.remove("spacebar-active")
+    buttonFinish.classList.remove("spacebar-active")
+}
--- scripts/stopwatch/stopwatch.js
+++ scripts/stopwatch/stopwatch.js
@@ -7,17 +7,11 @@ import * as display from "./display.js"
 import * as watches from "./watches.js"
 import * as trainingMode from "./training-mode.js"
 import * as timerMode from "./timer-mode.js"
+import * as spacebar from "./spacebar.js"
 
 const settingsButton = document.querySelector(".ts-settings")
 
-const buttonStart = document.querySelector(".st-start")
-const buttonStop = document.querySelector(".st-stop")
-const buttonContinue = document.querySelector(".st-continue")
-const buttonNext = document.querySelector(".st-next")
-const buttonFinish = document.querySelector(".st-finish")
-
 let trainingData = null
-let trainingInProgress = false
 
 
 // --- Public ---
@@ -50,71 +44,19 @@ export function destroy() {
 }
 
 export async function main() {
-    trainingInProgress = true
+    spacebar.activate()
 
     if (trainingData) await trainingMode.eventCycle()
     else              await timerMode.eventCycle()
 
-    keyUp({code: "Space"})      // Force keyUp on end, button actions are
-    trainingInProgress = false                   // ignored at this point
-
+    spacebar.deactivate()
     transitionToInitScreen()
 }
 
 
 // --- Private ---
 
-document.addEventListener("keydown", keyDown)
-document.addEventListener("keyup", keyUp)
 settingsButton.addEventListener("click", onSettingsClick)
-
-
-// Space bar button activation
-
-function keyDown(ev) {      // Activates button press appearance
-    if (!trainingInProgress || ev.code != "Space") return
-
-    if (getComputedStyle(buttonStart).display == "block") {
-        buttonStart.classList.add("spacebar-active")
-    }
-
-    if (getComputedStyle(buttonStop).display == "block") {
-        buttonStop.classList.add("spacebar-active")
-    }
-
-    if (getComputedStyle(buttonContinue).display == "block") {
-        buttonContinue.classList.add("spacebar-active")
-    }
-
-    if (getComputedStyle(buttonNext).display == "block") {
-        buttonNext.classList.add("spacebar-active")
-    }
-
-    if (getComputedStyle(buttonFinish).display == "block") {
-        buttonFinish.classList.add("spacebar-active")
-    }
-}
-
-function keyUp(ev) {
-    if (!trainingInProgress || ev.code != "Space") return
-
-    // Remove button press appearance even if button is no longer visible
-    buttonStart.classList.remove("spacebar-active")
-    buttonStop.classList.remove("spacebar-active")
-    buttonContinue.classList.remove("spacebar-active")
-    buttonNext.classList.remove("spacebar-active")
-    buttonFinish.classList.remove("spacebar-active")
-
-    // Activate button
-    if (getComputedStyle(buttonStart).display == "block") buttonStart.click()
-    if (getComputedStyle(buttonStop).display == "block") buttonStop.click()
-    if (getComputedStyle(buttonContinue).display == "block") buttonContinue.click()
-    if (getComputedStyle(buttonNext).display == "block") buttonNext.click()
-    if (getComputedStyle(buttonFinish).display == "block") buttonFinish.click()
-}
-
-
-// Changing settings
 
 async function onSettingsClick() {
     const changed = await settings.open()           // Can't be changed in
