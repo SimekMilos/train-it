17a24e041d3e4fc153b9e86372f29fff6970ae83
Miloš Šimek <tjsnake@me.com>
2021-10-16 20:47:13 +0200

    Adds displaying of Group

--- scripts/training/group.js
+++ scripts/training/group.js
@@ -1,6 +1,49 @@
 
+import {sizeNotes} from "../tools.js"
+
+const groupTemplate = document.querySelector(".tc-group-template")
+const nogroupTemplate = document.querySelector(".tc-no-group-template")
+
+
 export class Group {
     constructor(groupData, container) {
+        let group, groupContainer
 
+        // Group
+        if (groupData.type == "group") {
+            group = groupTemplate.content.cloneNode(true)
+            groupContainer = group.querySelector(".tcg-container")
+            this._notesButton = group.querySelector(".tcg-notes-button")
+            this._notes = group.querySelector(".tcg-notes")
+
+            // Heading
+            const heading = group.querySelector("h3")
+            heading.textContent = groupData.name
+
+            // Notes
+            if (groupData.notes) {
+                this._notes.value = groupData.notes
+                this._displayNotes()
+
+                // Note sizing on display
+                const observer = new ResizeObserver(() => {
+                    sizeNotes(this._notes)
+                    observer.disconnect()
+                })
+                observer.observe(this._notes)
+            }
+
+        // No Group
+        } else {
+            group = nogroupTemplate.content.cloneNode(true)
+            groupContainer = group.querySelector(".tc-no-group")
+        }
+
+        container.append(group)
+    }
+
+    _displayNotes() {
+        this._notesButton.style.display = "none"
+        this._notes.style.display = "block"
     }
 }
