f93aa4ffc521e864404528528bc9c2b5ab8f8a37
Miloš Šimek <tjsnake@me.com>
2021-10-16 22:38:22 +0200

    Adds displaying of Exercise

--- scripts/training/exercise.js
+++ scripts/training/exercise.js
@@ -1,6 +1,47 @@
 
+import {sizeNotes} from "../tools.js"
+
+const exerciseTemplate = document.querySelector(".tc-exercise-template")
+const notesTemplate = document.querySelector(".tc-notes-template")
+
+
 export class Exercise {
     constructor(exerciseData, container) {
+        const exerciseFrag = exerciseTemplate.content.cloneNode(true)
+        const exercise = exerciseFrag.firstElementChild
+        const exerciseContainer = exercise.querySelector(":scope .tce-container")
 
+        // Heading
+        const heading = exercise.querySelector(":scope .tce-name")
+        heading.textContent = exerciseData.name
+
+        // Notes
+        const notesButton = exercise.querySelector(":scope .tce-notes-button")
+
+        if (exerciseData.notes) {
+            const notesFrag = notesTemplate.content.cloneNode(true)
+            const notes = notesFrag.firstElementChild
+            notes.value = exerciseData.notes
+            exerciseContainer.append(notesFrag)
+
+            notesButton.style.display = "none"
+
+            // Note sizing on display
+            const observer = new ResizeObserver(() => {
+                sizeNotes(notes)
+                observer.disconnect()
+
+                // resize group here
+            })
+            observer.observe(notes)
+        }
+
+        // Notes sizing
+        // notes.addEventListener("input", sizeTrainingNotes)
+
+        // Sets
+
+
+        container.append(exerciseFrag)
     }
 }
