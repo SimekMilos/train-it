fe1d5f9ecac87d0eb65bc1a8c46f101cadd25ee9
Miloš Šimek <tjsnake@me.com>
2022-05-13 21:19:50 +0200

    Makes scrolling happen simultaneously with countdown

--- scripts/stopwatch/training-mode.js
+++ scripts/stopwatch/training-mode.js
@@ -212,7 +212,7 @@ async function next() {
 
     let precedeTime = 0, newPhase
     await waitForTick()
-    await training.scrollNextPhaseIntoView()
+    training.scrollNextPhaseIntoView()
 
     // Display set countdown
     if (display.watches.mode == "pause" && settings.getSetCountdown()) {
